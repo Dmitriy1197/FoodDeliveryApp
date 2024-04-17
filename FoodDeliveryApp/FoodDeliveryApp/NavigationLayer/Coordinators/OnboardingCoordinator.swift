cb7f230924463f1f80bc110af0fb15d55e01aa0b
Dmitriy1197 <dmitriy.gavrilyuk.1999@gmail.com>
2024-04-17 10:32:10 +0300

    Update onboarding flow

--- FoodDeliveryApp/FoodDeliveryApp/NavigationLayer/Coordinators/OnboardingCoordinator.swift
+++ FoodDeliveryApp/FoodDeliveryApp/NavigationLayer/Coordinators/OnboardingCoordinator.swift
@@ -20,17 +20,25 @@ private extension OnboardingCoordinator{
     func showOnboarding(){
         var pages = [UIViewController]()
         
-        let firstVC = UIViewController()
-        firstVC.view.backgroundColor = .purple
+        let firstVC = OnboardingPartViewController()
+        firstVC.imageToShow = UIImage(resource: .chickenLeg1)
+        firstVC.titleText = "Delicious Food"
+        firstVC.descriptionText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
         
-        let secondVC = UIViewController()
-        secondVC.view.backgroundColor = .green
+        let secondVC = OnboardingPartViewController()
+        secondVC.imageToShow = UIImage(resource: .shipped1)
+        secondVC.titleText = "Fast Shipping"
+        secondVC.descriptionText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Interdum rhoncus nulla."
         
-        let thirdVC = UIViewController()
-        thirdVC.view.backgroundColor = .yellow
+        let thirdVC = OnboardingPartViewController()
+        thirdVC.imageToShow = UIImage(resource: .medal1)
+        thirdVC.titleText = "Certificate Food"
+        thirdVC.descriptionText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ultricies mauris a id."
         
-        let fourthVC = UIViewController()
-        fourthVC.view.backgroundColor = .blue
+        let fourthVC = OnboardingPartViewController()
+        fourthVC.imageToShow = UIImage(resource: .creditCard1)
+        fourthVC.titleText = "Payment Online"
+        fourthVC.descriptionText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dui ultricies sit massa."
         
         pages.append(firstVC)
         pages.append(secondVC)
