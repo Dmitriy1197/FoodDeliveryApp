a32ddc6b18e461afe05984475c4142e12b034029
Dmitriy1197 <dmitriy.gavrilyuk.1999@gmail.com>
2024-04-19 12:07:45 +0300

    Added bottomView

---
+++ FoodDeliveryApp/FoodDeliveryApp/UILayer/AuthScene/Controllers/AuthViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AuthViewController.swift
+//  FoodDeliveryApp
+//
+//  Created by Dima on 19.04.2024.
+//
+
+import UIKit
+
+class AuthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+    
+}
